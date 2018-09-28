745621a45ff7e39e22cc9d819e4852ae7e260a37
冯天乐 <fengtianle@qq.com>
2018-09-28 14:24:55 +0800

    修改登录目录

--- CRM/app/Http/Controllers/RecordsController.php
+++ CRM/app/Http/Controllers/RecordsController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class RecordsController extends Controller
 {
-    //
+    # 展示跟单列表
+    public function recordsList(){
+        return view( 'records/recordsList' );
+    }
 }
